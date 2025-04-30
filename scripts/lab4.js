2ae6f7c4c684e3d469351f55dde4baa1da9de22e
Eric Nguyen <ern005@ucsd.edu>
2025-04-29 20:46:09 -0700

    Fixed bugs in the scripts

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -7,14 +7,12 @@
  */
 function sumValues(num1, num2, add) {
     if (add) {
-        const result = 0;
-
-        result = num1 + num2;
+        const result = num1 + num2;
 
         return result;
     }
     else {
-        return !add;
+        return false;
     }
 }
 
@@ -27,9 +25,12 @@ function sumValues(num1, num2, add) {
 function discountPrices(prices, discount) {
     const discounted = []
     const length = prices.length;
-    let discountedPrice = 0
+    if (length === 0) {
+        return false;
+    }
+
     for(let i = 0; i < length; i++) {
-        discountedPrice += prices[i] * (1 - discount);
+        const discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
 
